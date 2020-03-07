e3c71777b8d1d361e18fb35e628affb6ccd810b8
scaraclette <gustiscarlett@gmail.com>
2020-03-07 11:01:44 -0800

    regular server working

--- sg/settings.py
+++ sg/settings.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = 'sg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'sg',
+        'USER': 'sguser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
